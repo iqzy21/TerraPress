ef3b91c0b735f2951db3572a0a9c5d3f692beea8
iqzy21 <iqbaal.el21@gmail.com>
2025-11-17 17:44:39 +0000

    G: Created VPC security group

---
+++ wordpressSG.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "wordpress" {
+  name        = "wordpressSG"
+  description = "Allow EC2 wordpress inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.wordpress.id
+  tags = {
+    Name = "wordpressSG"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ssh" {
+  security_group_id = aws_security_group.wordpress.id
+
+  cidr_ipv4   = "92.235.135.93/32"
+  from_port   = 22
+  ip_protocol = "tcp"
+  to_port     = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "inbound_http" {
+  security_group_id = aws_security_group.wordpress.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
+
+resource "aws_vpc_security_group_egress_rule" "outbound_http" {
+  security_group_id = aws_security_group.wordpress.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = 80
+  ip_protocol = "tcp"
+  to_port     = 80
+}
